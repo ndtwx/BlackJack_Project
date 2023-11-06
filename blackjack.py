3fe2b81c1b4a4218c08592c331a1ffb5dd43d0f3
ndtwx <nd_twx@hotmail.com>
2023-11-07 04:16:33 +0800

    replace file

--- blackjack.py
+++ blackjack.py
@@ -16,7 +16,7 @@ def calculate_score(cards):
    """Take a list of cards and return the score calculated from the cards"""
     #Check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in the game.
    if sum(cards) == 21 and len(cards) == 2:
-        return 0
+        return 21
    #Check for an 11 (ace). If the score is already over 21, remove the 11 and replace it with a 1. 
    if 11 in cards and sum(cards) > 21:
       cards.remove(11)
@@ -57,8 +57,10 @@ def play_game():
         for _ in range(2):
             user_cards.append(deal_card())
             computer_cards.append(deal_card())
+            
+        user_score = calculate_score(user_cards)
 
-        return render_template('play.html', user_cards=user_cards, computer_card=computer_cards[0])
+        return render_template('play.html', logo=logo, user_cards=user_cards, computer_card=computer_cards[0], user_score=user_score)
 
     if request.method == 'POST':
         # Handle user's actions (draw or pass)
@@ -68,9 +70,9 @@ def play_game():
             computer_score = calculate_score(computer_cards)
 
             if user_score > 21:
-                return render_template('result.html', user_cards=user_cards, computer_cards=computer_cards, result="YOU WENT OVER! YOU LOSE!", 
+                return render_template('result.html', logo=logo, user_cards=user_cards, computer_cards=computer_cards, result="YOU WENT OVER! YOU LOSE!", 
                                         user_score=user_score, computer_score=computer_score)
-            return render_template('play.html', user_cards=user_cards, computer_card=computer_cards[0])
+            return render_template('play.html', logo=logo, user_cards=user_cards, computer_card=computer_cards[0], user_score=user_score )
         else:
             # Computer's turn to play
             while calculate_score(computer_cards) < 17:
@@ -80,7 +82,7 @@ def play_game():
             computer_score = calculate_score(computer_cards)
             result = compare(user_score, computer_score)
 
-            return render_template('result.html', user_cards=user_cards, user_score=user_score, computer_score=computer_score, 
+            return render_template('result.html', logo=logo, user_cards=user_cards, user_score=user_score, computer_score=computer_score, 
                                    computer_cards=computer_cards, result=result)
 
 if __name__ == '__main__':
